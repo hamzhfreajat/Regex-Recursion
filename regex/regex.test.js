a642568fbf503cd9821f090ff7f7d23bd6d35e8e
hamzhfreajat <97644709+hamzhfreajat@users.noreply.github.com>
2022-01-28 00:42:31 +0200

    Update regex.test.js

--- regex/regex.test.js
+++ regex/regex.test.js
@@ -16,7 +16,7 @@ function capitalA(s){
 which end with io (example@example.io) */
 
 function ioEmail(email){
-    let regex = /^[^\s@.]+@[^\s@]+\.[^\s@]+[^\sgmail]/mg
+    let regex = /^([a-zA-Z0-9_\-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/mg
     return regex.test(email);
 }
 
